e2f77bd117b8e5d1adaa9b766a1e90f705e0aecd
bramprasetyop <bramprasetyop@gmail.com>
2020-08-14 17:16:42 +0700

    pokedex app fe test gokomodo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import GKHome from './containers/GKHome';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <GKHome />   
+    )
+  }
+}
+
+export default App
